33152b375c0e236ed62bcd0fa24e24e60d064d80
paytkovsg <paytkov.s@gmail.com>
2021-11-27 14:46:38 +0500

    исправление типа вывода на plain

--- src/main/java/ru/gb/servlets/ProductServlet.java
+++ src/main/java/ru/gb/servlets/ProductServlet.java
@@ -16,7 +16,12 @@ public class ProductServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-//        resp.setHeader("Content-Type", "text/html; charset=utf-8");
+        /*
+        По заданию все верно - оценка отлично.
+По Вашему вопросу: с кодировкой все верно, правда когда Вы указали Content-Type text/html, то браузер обернул ответ от сервера в недостающие html теги. А так как html игнорирует переносы строк (для этого нужен тег br), то получается что в браузере Вы видите html страничку в таком виде - где товары в одну строку.
+Можете поменять тип возвращаемого ответа "text/plain; charset=utf-8" - так Вы сообщаете, что это просто текст, не html, и он будет в итоге отображен как обычный текст, со своими переносами
+         */
+        resp.setHeader("Content-Type", "text/plain; charset=utf-8");
 
 
 
